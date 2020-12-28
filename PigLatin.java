ae15be3a86261e255c6518aae863f25b50c76e0b
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-12-28 18:04:43 -0500

    pig latin 2 works

--- PigLatin.java
+++ PigLatin.java
@@ -5,6 +5,12 @@ public class PigLatin {
     System.out.println(PigLatinSimple("pie"));
     System.out.println(PigLatinSimple("david"));
     System.out.println(PigLatinSimple("aaron"));
+
+    System.out.println(pigLatin("the"));
+    System.out.println(pigLatin("check"));
+    System.out.println(pigLatin("skee"));
+    System.out.println(pigLatin("emu"));
+    System.out.println(pigLatin("grade"));
   }
 
   public static String PigLatinSimple(String s){
@@ -18,6 +24,25 @@ public class PigLatin {
       s = pig;
     }
     return s;
+  }
+
+  public static String pigLatin(String s) {
+    s = s.toLowerCase();
+    if (s.substring(0,2).equals("bl") || s.substring(0,2).equals("br") || s.substring(0,2).equals("ch") ||
+    s.substring(0,2).equals("ck") || s.substring(0,2).equals("cl") || s.substring(0,2).equals("cr") ||
+    s.substring(0,2).equals("dr") || s.substring(0,2).equals("fl") || s.substring(0,2).equals("fr") ||
+    s.substring(0,2).equals("gh") || s.substring(0,2).equals("gl") || s.substring(0,2).equals("gr") ||
+    s.substring(0,2).equals("ng") || s.substring(0,2).equals("ph") || s.substring(0,2).equals("pl") ||
+    s.substring(0,2).equals("pr") || s.substring(0,2).equals("qu") || s.substring(0,2).equals("sc") ||
+    s.substring(0,2).equals("sh") || s.substring(0,2).equals("sk") || s.substring(0,2).equals("sl") ||
+    s.substring(0,2).equals("sm") || s.substring(0,2).equals("sn") || s.substring(0,2).equals("sp") ||
+    s.substring(0,2).equals("st") || s.substring(0,2).equals("sw") || s.substring(0,2).equals("th") ||
+    s.substring(0,2).equals("tw") || s.substring(0,2).equals("wh") || s.substring(0,2).equals("wr")) {
+      String f = s.substring(0,2);
+      s = s.substring(2) + f + "ay";
+      return s;
+    }
+    else return PigLatinSimple(s);
   }
 
 }
