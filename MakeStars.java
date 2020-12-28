2016f0d011eb573447d47e8a9e794300e3caf902
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-12-28 16:54:24 -0500

    stars work

--- MakeStars.java
+++ MakeStars.java
@@ -2,6 +2,16 @@ import java.util.*;
 public class MakeStars {
   public static void main(String[] args) {
     Scanner n = new Scanner( System.in );
-    
+    while (n.hasNextLine()) {
+      Scanner w = new Scanner(n.nextLine());
+      while (w.hasNext()) {
+        String c = w.next();
+        for (int i = 0; i < c.length(); i++) {
+          System.out.print("*");
+        }
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
   }
 }
